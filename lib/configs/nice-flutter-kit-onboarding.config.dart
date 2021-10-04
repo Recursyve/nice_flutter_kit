25f42bf05c998d5424d610cec736b403308161f2
MacChab <32344757+MacChab@users.noreply.github.com>
2021-10-04 16:21:21 -0400

    wip

--- lib/configs/nice-flutter-kit-onboarding.config.dart
+++ lib/configs/nice-flutter-kit-onboarding.config.dart
@@ -2,24 +2,24 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class NiceFlutterKitOnboardingConfig {
   final String sharedPrefKey;
-  static bool? _onboardingCompleted;
+  bool? _onboardingCompleted;
   late SharedPreferences _sharedPref;
 
   NiceFlutterKitOnboardingConfig({this.sharedPrefKey: "ONBOARDING_COMPLETED"});
 
   Future<void> init() async {
     _sharedPref = await SharedPreferences.getInstance();
-    _onboardingCompleted = await _sharedPref.getBool(sharedPrefKey) ?? false;
+    this._onboardingCompleted = _sharedPref.getBool(sharedPrefKey) ?? false;
   }
 
   bool get onboardingCompleted {
-    assert(_onboardingCompleted == null);
-    return _onboardingCompleted!;
+    assert(this._onboardingCompleted != null);
+    return this._onboardingCompleted!;
   }
 
   set onboardingCompleted(bool onboardingCompleted) {
-    assert(_onboardingCompleted != null);
-    _onboardingCompleted = onboardingCompleted;
+    assert(this._onboardingCompleted != null);
+    this._onboardingCompleted = onboardingCompleted;
     _sharedPref.setBool(sharedPrefKey, true);
   }
 }

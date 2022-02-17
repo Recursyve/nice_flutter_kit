a5f9ce1a3054abc1ddd8dce3a14d354d8572aaf0
Sandra <sandra@recursyve.io>
2022-02-17 17:54:33 -0500

    Add export nice-automatic-keep-alive-client

--- lib/src/widgets/public.dart
+++ lib/src/widgets/public.dart
@@ -4,3 +4,4 @@ export 'nice-button.widget.dart';
 export 'nice-error.widget.dart';
 export 'nice-loading-overlay.widget.dart';
 export 'nice-text-accent-punctuation.widget.dart';
+export 'nice-automatic-keep-alive-client.widget.dart';

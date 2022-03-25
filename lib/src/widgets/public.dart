c4fdbcc133153b0942978ceca55b0d25a7646e69
samuelgirardpaquet <48529772+samuelgirardpaquet@users.noreply.github.com>
2022-03-25 11:05:35 -0400

    add nice-password-validator (#16)
    * added nice-password-validator
    
    * changed changes requested
    
    * added reactive
    
    * changes after review
    
    * changed changes requested

--- lib/src/widgets/public.dart
+++ lib/src/widgets/public.dart
@@ -6,3 +6,4 @@ export 'nice-button.widget.dart';
 export 'nice-error.widget.dart';
 export 'nice-loading-overlay.widget.dart';
 export 'nice-text-accent-punctuation.widget.dart';
+export 'reactive-checklist-validator.widget.dart';

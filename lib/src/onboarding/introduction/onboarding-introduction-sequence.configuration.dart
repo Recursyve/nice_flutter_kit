01529fa8f567ff67c8944043e3f541eae36a11a0
Marc-André Chabot <32344757+MacChab@users.noreply.github.com>
2022-07-21 12:45:01 -0400

    Pouvoir customiser le dots indicator et les boutons next et skip de la onboardingIntroductionSequence. (#29)
    * done
    
    * fix
    
    * fixes

--- lib/src/onboarding/introduction/onboarding-introduction-sequence.configuration.dart
+++ lib/src/onboarding/introduction/onboarding-introduction-sequence.configuration.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 import 'package:nice_flutter_kit/nice_flutter_kit.dart';
 
 class NiceOnboardingIntroductionConfiguration {
@@ -17,11 +19,31 @@ class NiceOnboardingIntroductionSequenceConfiguration {
   final String skip;
   final String next;
   final String done;
+  final NiceOnboardingIntroductionSequenceFooterConfig footerConfig;
 
   const NiceOnboardingIntroductionSequenceConfiguration({
     required this.configurations,
     required this.skip,
     required this.next,
     required this.done,
+    this.footerConfig = const NiceOnboardingIntroductionSequenceFooterConfig(),
   }) : assert(configurations.length > 0);
+}
+
+class NiceOnboardingIntroductionSequenceFooterConfig {
+  final DotsDecorator? dotsDecorator;
+  final int dotsFlex;
+  final int nextFlex;
+  final int skipOrBackFlex;
+  final ButtonStyle buttonStyle;
+
+  const NiceOnboardingIntroductionSequenceFooterConfig({
+    this.dotsDecorator,
+    this.dotsFlex = 1,
+    this.nextFlex = 1,
+    this.skipOrBackFlex = 1,
+    this.buttonStyle = const ButtonStyle(),
+  })  : assert(dotsFlex > 0),
+        assert(nextFlex > 0),
+        assert(skipOrBackFlex > 0);
 }

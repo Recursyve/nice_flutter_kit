bcae288022b1057218c193ad7a54c45c1c080aa8
Gabriel Beaudry <gabriel.bdry@gmail.com>
2022-09-14 13:45:23 -0400

    Le layout de onboarding avait des problèmes d'overflow entre autres avec les boutons de permission. (#34)
    Co-authored-by: Jtplouffe <jtplouffe@gmail.com>

--- lib/src/onboarding/introduction/onboarding-introduction.wrapper.dart
+++ lib/src/onboarding/introduction/onboarding-introduction.wrapper.dart
@@ -31,21 +31,19 @@ class NiceOnboardingIntroductionSequence extends StatelessWidget {
       pages: [
         for (final configuration in sequenceConfiguration.configurations)
           PageViewModel(
-            image: _buildImage(configuration.imageUrl),
+            image: Padding(
+              padding: const EdgeInsets.only(top: 64.0),
+              child: _buildImage(configuration.imageUrl),
+            ),
             titleWidget: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: Column(
-                children: [
-                  const SizedBox(height: 40),
-                  configuration.title,
-                ],
-              ),
+              padding: const EdgeInsets.only(top: 40.0, left: 24.0, right: 24.0),
+              child: configuration.title,
             ),
             decoration: PageDecoration(
               bodyAlignment: Alignment.topCenter,
               imageAlignment: Alignment.center,
-              imageFlex: 3,
-              bodyFlex: 2,
+              imageFlex: 2,
+              bodyFlex: 3,
               imagePadding: const EdgeInsets.only(bottom: 0),
               pageColor: Theme.of(context).backgroundColor,
               contentMargin: const EdgeInsets.all(5),

c2666f9d278b14825c97bf1431e922d6969f175a
Jtplouffe <jtplouffe@gmail.com>
2022-10-20 12:31:12 -0400

    Added default box decoration to search bar

--- lib/src/base-list/widgets/base-list-search-bar.widget.dart
+++ lib/src/base-list/widgets/base-list-search-bar.widget.dart
@@ -19,7 +19,7 @@ class NiceBaseListSearchBar<D> extends StatefulWidget {
   final Duration debounceDuration;
 
   const NiceBaseListSearchBar({
-    required this.decoration,
+    this.decoration: const BoxDecoration(),
     this.inputDecoration: const InputDecoration(),
     this.debounceDuration: const Duration(milliseconds: 300),
   });

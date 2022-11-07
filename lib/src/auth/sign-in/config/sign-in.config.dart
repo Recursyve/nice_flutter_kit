c3cb52989975fb242c3a88182d8244a0c8cec7de
samuel <samuelgirardpaquet@gmail.com>
2022-11-07 16:26:52 -0500

    add validators to config

--- lib/src/auth/sign-in/config/sign-in.config.dart
+++ lib/src/auth/sign-in/config/sign-in.config.dart
@@ -1,8 +1,14 @@
+import 'package:reactive_forms/reactive_forms.dart';
+
 class NiceSignInConfig {
   /// Whether or not to activate native autofill for the email and password, and save it afterwards.
   final bool autofillEmailAndPassword;
 
+  /// List of validators that the email field will be using.
+  final List<ValidatorFunction>? signInEmailFieldValidators;
+
   const NiceSignInConfig({
     this.autofillEmailAndPassword: false,
+    this.signInEmailFieldValidators,
   });
 }

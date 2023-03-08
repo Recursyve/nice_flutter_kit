e441a13e851ad88b7adc525eb03e22e25793a70f
Gabriel Beaudry <gabriel.bdry@gmail.com>
2023-03-07 19:03:19 -0500

    Correctif pour quand l'utilisateur ferme le clavier manuellement. (#47)
    * Fix for field unfocus widget when closing keyboard manually
    
    * Fixes
    
    * Change implementation
    
    * Remove unused

--- lib/src/widgets/public.dart
+++ lib/src/widgets/public.dart
@@ -1,7 +1,7 @@
-export 'automatic-unfocus.widget.dart';
 export 'base-list/public.dart';
 export 'nice-animated-fade-in.widget.dart';
 export 'nice-automatic-keep-alive-client.widget.dart';
+export 'nice-automatic-unfocus.widget.dart';
 export 'nice-error.widget.dart';
 export 'nice-loading-overlay.widget.dart';
 export 'nice-text-accent-punctuation.widget.dart';

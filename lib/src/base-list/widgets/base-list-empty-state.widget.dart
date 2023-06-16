323f86b735f611a8759f112496834c36b4200868
Jtplouffe <jtplouffe@gmail.com>
2023-06-16 11:07:06 -0400

    document widget

--- lib/src/base-list/widgets/base-list-empty-state.widget.dart
+++ lib/src/base-list/widgets/base-list-empty-state.widget.dart
@@ -1,6 +1,15 @@
 import "package:flutter/widgets.dart";
 import "package:nice_flutter_kit/nice_flutter_kit.dart";
 
+/// This widget is part of the base list, and MUST be a child of [NiceBaseListConfig].
+///
+/// The [D] generic type refers to the filtered type (e.g. `Accounts`). It must be provided and it must be the same type
+/// as the [NiceBaseListConfig].
+///
+/// This widget is used to show an empty state whenever the list has no items.
+/// The empty state will only be showed if both of the following conditions are true:
+/// - The list has no values
+/// - The list has initialized and the first load has been completed
 class NiceBaseListEmptyState<D> extends StatelessWidget {
   final WidgetBuilder emptyStateBuilder;
   final Widget? child;

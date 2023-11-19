00282a38d0cc9901a2cabbee6b722016b78497f2
Jtplouffe <32107801+Jtplouffe@users.noreply.github.com>
2023-11-19 11:41:43 -0500

    Finished

--- lib/src/auth/sign-in/public.dart
+++ lib/src/auth/sign-in/public.dart
@@ -1,6 +1,4 @@
 export "./cubit/sign-in.cubit.dart";
 export "./cubit/sign-in.state.dart";
 export "./password/public.dart";
-export "./social/public.dart";
-export "./widgets/sign-in-errors.widget.dart";
-export "./widgets/sign-in.widget.dart";
+export "./widgets/public.dart";

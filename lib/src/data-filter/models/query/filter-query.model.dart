831d396b239e9b84ffbd552fe4307af3132eb578
Gabriel Audette <gaudette@recursyve.io>
2024-01-31 14:48:24 -0500

    Added rules utility method

--- lib/src/data-filter/models/query/filter-query.model.dart
+++ lib/src/data-filter/models/query/filter-query.model.dart
@@ -1,31 +1,18 @@
+import "package:collection/collection.dart";
 import "package:equatable/equatable.dart";
 import "package:nice_flutter_kit/nice_flutter_kit.dart";
 
 class NiceFilterQueryModel extends NiceBaseFilterQueryModel with EquatableMixin {
   final NiceFilterQueryConditionType condition;
-  final List<NiceBaseFilterQueryModel> rules;
+  final List<NiceFilterQueryRuleModel> rules;
 
   const NiceFilterQueryModel({
     required this.condition,
     required this.rules,
   });
 
-  // Recursively loop through the rules to maybe find a rule with id
   D? findValueForId<D>(String id) {
-    for (final rule in rules) {
-      if (rule is NiceFilterQueryModel) {
-        final value = rule.findValueForId(id);
-        if (value != null) {
-          return value;
-        }
-      } else if (rule is NiceFilterQueryRuleModel) {
-        if (rule.id == id) {
-          return rule.value;
-        }
-      }
-    }
-
-    return null;
+    return rules.firstWhereOrNull((rule) => rule.id == id)?.value as D?;
   }
 
   @override
